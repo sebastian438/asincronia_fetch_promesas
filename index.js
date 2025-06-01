318e409e78fbf6a8b7512c536d28f8564f4b7163
sebastian438 <sebastian07051999@gmail.com>
2025-06-01 15:04:30 +0200

    ADD: ejercico 02 resuelto

--- index.js
+++ index.js
@@ -3,17 +3,31 @@
 
 //EJERCICIO 01
 
-urlBase01 = "https://dog.ceo/api/breeds/list/all"
+// urlBase01 = "https://dog.ceo/api/breeds/list/all"
 
-const getAllBreeds = async () => {
-    const response = await fetch(`${urlBase01}`);
+// const getAllBreeds = async () => {
+//     const response = await fetch(`${urlBase01}`);
+//     let data;
+//     let lista = []
+//     if (response.ok) {
+//         data = await response.json();
+//         let razas = data.message;
+//         let llaves = Object.keys(razas);
+//         return llaves;
+//     }
+// }
+
+
+//EJERCIO 02
+
+const getRandomDog = async () => {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+
     let data;
-    let lista = []
     if (response.ok) {
         data = await response.json();
-        let razas = data.message;
-        let llaves = Object.keys(razas);
-        return llaves;
+        let imagen = data.message;
+        return imagen;
     }
 }
 
